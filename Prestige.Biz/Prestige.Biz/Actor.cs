ab3a27dba0cef683b1f8d5a713adb32f7ce48f78
Richard Mutembereza <Richard.Mutembereza@derivco.co.za>
2020-03-10 10:09:44 +0200

    Set up business layer

---
+++ Prestige.Biz/Prestige.Biz/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestige.Biz
+{
+    class Actor
+    {
+      public string GetOccupation()
+      {
+        return "Actor";
+      }
+    }
+}
