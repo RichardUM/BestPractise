b7d8e07f613ec0af704ef1246eec2e3823cce2b1
Richard Mutembereza <Richard.Mutembereza@derivco.co.za>
2020-03-11 09:34:13 +0200

    Running unit tests

--- Prestige.Biz/Prestige.Biz/Actor.cs
+++ Prestige.Biz/Prestige.Biz/Actor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Prestige.Biz
 {
-    class Actor
+  public class Actor
     {
       public string GetOccupation()
       {
---
+++ Prestige.Biz/Prestige.BizTests/ActorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prestige.Biz;
+
+
+namespace Prestige.BizTests
+{
+    [TestClass]
+    public class ActorTest
+    {
+        [TestMethod]
+        public void TestGetOccupation()
+        {
+          var currentActor = new Actor();
+          var expected = "Actor";
+
+          string result = currentActor.GetOccupation();
+
+          Assert.AreEqual(expected,result);
+        }
+    }
+}
