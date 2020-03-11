ac736888ee520675601c5b76462abab905c7b282
Richard Mutembereza <Richard.Mutembereza@derivco.co.za>
2020-03-11 11:17:06 +0200

    Static classes

---
+++ Prestige.Biz/Prestige.Common/NotificationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prestige.Common
+{
+  static public class NotificationService
+  {
+    /// <summary>
+    /// Notifies Talent 
+    /// </summary>
+    /// <param name="talent"></param>
+    /// <returns></returns>
+    static public string NotifyTalent(string talent)
+    {
+      var message = "talent: " + talent;
+      Console.WriteLine(message);
+      return message;
+
+    }
+  }
+}
